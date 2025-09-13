89e3f2d80bc4d36161600955efc25b3ada255a51
Omar-Yasser-Frontend <craft14716@gmail.com>
2025-09-13 16:58:03 +0300

    feat: adding change password, forgot password, reset password routes

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -67,3 +67,62 @@ exports.confirmUser = async (req, res) => {
 
   ResponseFormatter.success(res, null, "Account verified", 200);
 };
+
+exports.forgotPassword = async (req, res) => {
+  const { email } = req.body;
+  const user = await authService.findOne({ email }, "user");
+  const { token, hashedToken } = cryptoToken();
+
+  user.resetPassword.token = hashedToken;
+  user.resetPassword.expiresAt = Date.now() + 60 * 15 * 1000;
+  await user.save();
+
+  sendEmail(
+    `${process.env.CLIENT_URL}/reset-password?token=${token}&userId=${user._id}`
+  );
+
+  ResponseFormatter.success(res, null, "Check your email to reset password");
+};
+
+exports.resetPassword = async (req, res) => {
+  const { token, userId, password } = req.body;
+  const user = await authService.findById(userId, "user");
+  const hashToken = crypto.createHash("sha256").update(token).digest("hex");
+
+  if (user.resetPassword.expiresAt < Date.now())
+    throw new AppError("Token Expired", 400);
+
+  if (hashToken !== user.resetPassword.token)
+    throw new AppError("Invalid magic link", 403);
+
+  user.password = password;
+  user.resetPassword = undefined;
+  await user.save();
+
+  ResponseFormatter.success(
+    res,
+    null,
+    "Password changed successfully, please login",
+    200
+  );
+};
+
+exports.changePassword = async (req, res) => {
+  const { userId } = req.user;
+  const { currentPassword, newPassword } = req.body;
+  const user = await authService.findById(userId, "user");
+
+  if (!(await user.comparePassword(currentPassword)))
+    throw new AppError("Invalid Password", 403);
+
+  user.password = newPassword;
+  await user.save();
+
+  ResponseFormatter.success(res, null, "Password changed successfully", 200);
+};
+
+exports.logout = (req, res) => {
+  res.clearCookie("refreshToken");
+
+  ResponseFormatter.success(res, null, null, 204);
+};
--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -10,4 +10,10 @@ router.post("/login", validate(authSchemas.login), authController.login);
 
 router.post("/confirmation", authController.confirmUser);
 
+router.post("/change-password", authController.changePassword);
+
+router.post("/forgot-password", authController.forgotPassword);
+
+router.post("/reset-password", authController.resetPassword);
+
 module.exports = router;
