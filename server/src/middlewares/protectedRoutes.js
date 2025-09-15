97a09cca0068d4262879a763aeddb8b57660e520
Omar-Yasser-Frontend <craft14716@gmail.com>
2025-09-15 20:28:04 +0300

    refactor: improve protectedRoutes middleware with more security best practices

--- server/src/middlewares/protectedRoutes.js
+++ server/src/middlewares/protectedRoutes.js
@@ -1,26 +1,37 @@
 const jwt = require("jsonwebtoken");
 const AppError = require("../core/AppError");
+const getUserProtectedRoutes = require("../utils/getUserProtectedRoutes");
+const createJWTToken = require("../utils/createJWTToken");
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
   const accessToken = req.headers["authorization"]?.split(" ")[1];
   const refreshToken = req.cookies["refreshToken"];
 
-  if (!accessToken && !refreshToken) throw new AppError("Not Authorized", 401);
+  if (!accessToken || !refreshToken) throw new AppError("Not Authorized", 401);
 
   try {
     const decoded = jwt.verify(accessToken, process.env.JWT_SECRET);
-    req.user = decoded;
+
+    const user = await getUserProtectedRoutes(decoded);
+
+    if (user.passwordChangeDate(decoded.iat))
+      throw new AppError("Password changed recently, please login again", 401);
+
+    req.user = user;
     next();
   } catch (err) {
     if (!refreshToken) throw new AppError("Not Authorized", 401);
+    if (err.name !== "TokenExpiredError") throw err;
 
     const decoded = jwt.verify(refreshToken, process.env.JWT_SECRET);
-    const newAccessToken = jwt.sign({ user: decoded.user }, secretKey, {
-      expiresIn: "15m",
-    });
+    const newAccessToken = createJWTToken(decoded, "15m");
+
+    const user = await getUserProtectedRoutes(decoded);
+    if (user.passwordChangeDate(decoded.iat))
+      throw new AppError("Password changed recently, please login again", 401);
 
     res.setHeader("Authorization", `Bearer ${newAccessToken}`);
-    req.user = decoded;
+    req.user = user;
     next();
   }
 };
