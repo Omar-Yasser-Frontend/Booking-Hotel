87f5025716b45d9f606790ea618ecc28a8de5938
Omar-Yasser-Frontend <craft14716@gmail.com>
2025-09-17 19:45:18 +0300

    feat: adding some routes configuration

--- server/src/app.js
+++ server/src/app.js
@@ -8,7 +8,13 @@ const rateLimit = require("express-rate-limit");
 const hpp = require("hpp");
 
 const roomRouter = require("./routes/rooms");
+const reviewsRouter = require("./routes/reviews");
+const wishlistRouter = require("./routes/wishlist");
+const userRouter = require("./routes/user");
 const authRouter = require("./routes/auth");
+const paymentRouter = require("./routes/payment");
+const webhookRouter = require("./routes/webhook");
+const reservationRouter = require("./routes/reservations");
 const notFoundError = require("./middlewares/notFoundError");
 const ResponseFormatter = require("./core/ResponseFormatter");
 const globalErrorHandler = require("./middlewares/globalErrorHandler");
@@ -18,7 +24,7 @@ const app = express();
 app.set("query parser", "extended");
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
@@ -38,8 +44,6 @@ app.use(helmet());
 
 app.use(cookieParser());
 
-app.use(express.json({ limit: "10kb" }));
-
 // allow modifing query property in req object
 // to prevent errors in mongoSanitize library and better query for mongoose
 app.use((req, res, next) => {
@@ -51,8 +55,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(mongoSanititze());
-
 app.use(hpp());
 
 if (process.env.NODE_ENV === "development") {
@@ -63,8 +65,19 @@ app.get("/", (req, res) => {
   ResponseFormatter.success(res, null, "Welcome to my Hotel App", 200);
 });
 
+app.use("/api/v1/webhook", webhookRouter);
+
+app.use(express.json({ limit: "10kb" }));
+
+app.use(mongoSanititze());
+
 app.use("/api/v1/room", roomRouter);
 app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/review", reviewsRouter);
+app.use("/api/v1/wishlist", wishlistRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/payment", paymentRouter);
+app.use("/api/v1/reservation", reservationRouter);
 
 app.all(/(.*)/, notFoundError);
 
