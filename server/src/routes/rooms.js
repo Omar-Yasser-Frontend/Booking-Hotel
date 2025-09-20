a0189fde004681d8c56ddf716f873a4a2f982a2b
Omar-Yasser-Frontend <craft14716@gmail.com>
2025-09-20 05:39:17 +0300

    feat: adding admin restriction on some routes

--- server/src/routes/rooms.js
+++ server/src/routes/rooms.js
@@ -4,7 +4,7 @@ import * as roomController from "../controllers/roomController.js";
 import protectedRoutes from "../middlewares/protectedRoutes.js";
 import validate from "../middlewares/validationMiddleware.js";
 import { roomSchema } from "../validation/roomsSchemas.js";
-import restrictTo from "../utils/restrictTo.js";
+import restrictTo from "../middlewares/restrictTo.js";
 
 const router = express.Router();
 
@@ -14,7 +14,7 @@ router.get("/search", roomController.searchRoom);
 
 router.get("/:id", roomController.getRoomById);
 
-router.use(protectedRoutes);
+router.use(protectedRoutes, restrictTo(["admin"]));
 
 router.post("/", validate(roomSchema), roomController.createRoom);
 
