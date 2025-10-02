a1f906b066e1c8d5cd90dc20cf569ea9c4cce90e
Omar-Yasser-Frontend <craft14716@gmail.com>
2025-10-02 16:59:57 +0300

    feat: adding some more feature in router and app.css

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -3,6 +3,15 @@ import { Routes, Route, useLocation } from "react-router";
 import HomePage from "./pages/HomePage";
 import PageLayout from "./layout/PageLayout";
 import MainLoading from "./components/MainLoading";
+import NotFound from "./pages/NotFound";
+import Login from "./pages/Login";
+import AuthLayout from "./features/auth/layouts/AuthLayout";
+import LoginSignupLayout from "./features/auth/layouts/LoginSignupLayout";
+import Signup from "./pages/Signup";
+import AccountVerification from "./pages/AccountVerification";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
+import Rooms from "./pages/Rooms";
 
 function Router() {
   const location = useLocation();
@@ -11,7 +20,17 @@ function Router() {
       <Routes>
         <Route element={<PageLayout />}>
           <Route index element={<HomePage />} />
-          <Route path="test" element={<p>test-page</p>} />
+          <Route path="/rooms" element={<Rooms />} />
+          <Route element={<AuthLayout />}>
+            <Route element={<LoginSignupLayout />}>
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+            </Route>
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/reset-password" element={<ResetPassword />} />
+          </Route>
+          <Route path="/confirm" element={<AccountVerification />} />
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </Suspense>
