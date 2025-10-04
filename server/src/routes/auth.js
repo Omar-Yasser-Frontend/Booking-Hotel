7838b453210e382d80b2e8768fa8970fb5bb76d6
Omar-Yasser-Frontend <craft14716@gmail.com>
2025-10-04 09:02:26 +0300

    feat: adding more rate limit for auth endpoints

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -3,8 +3,40 @@ import * as authController from "../controllers/authController.js";
 import validate from "../middlewares/validationMiddleware.js";
 import * as authSchemas from "../validation/authSchemas.js";
 import protectedRoutes from "../middlewares/protectedRoutes.js";
+import ratelimit from "express-rate-limit";
 
 const router = express.Router();
+
+router.post(
+  "/forgot-password",
+  ratelimit({
+    windowMs: 10 * 60 * 1000,
+    limit: 1,
+    standardHeaders: "draft-8",
+    legacyHeaders: false,
+    ipv6Subnet: 56,
+    message: {
+      status: "fail",
+      message: "Too many request, please try again after 10m",
+    },
+  }),
+  validate(authSchemas.forgotPassword),
+  authController.forgotPassword
+);
+
+router.use(
+  ratelimit({
+    windowMs: 60 * 60 * 1000,
+    limit: 10,
+    standardHeaders: "draft-8",
+    legacyHeaders: false,
+    ipv6Subnet: 56,
+    message: {
+      status: "fail",
+      message: "Too many request, please try again after 10m",
+    },
+  })
+);
 
 router.post("/signup", validate(authSchemas.createUser), authController.signup);
 
@@ -21,12 +53,6 @@ router.post(
   validate(authSchemas.changePassword),
   protectedRoutes,
   authController.changePassword
-);
-
-router.post(
-  "/forgot-password",
-  validate(authSchemas.forgotPassword),
-  authController.forgotPassword
 );
 
 router.post(
