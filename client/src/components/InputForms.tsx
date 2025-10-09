5858993b11756ba6d4cc7ca7bbbfed12bc71ff9c
Omar-Yasser-Frontend <craft14716@gmail.com>
2025-10-09 20:40:31 +0300

    refactor: adding more props to InputForms and PrimaryBtn

--- client/src/components/InputForms.tsx
+++ client/src/components/InputForms.tsx
@@ -7,6 +7,7 @@ interface InputProps {
   label: string;
   register: UseFormRegisterReturn;
   error: string | undefined;
+  onKeyDown?: (e: React.KeyboardEvent) => void;
 }
 
 function InputForm({
@@ -14,6 +15,7 @@ function InputForm({
   type = "text",
   placeholder,
   label,
+  onKeyDown,
   register,
   error,
 }: InputProps) {
@@ -25,6 +27,7 @@ function InputForm({
       <input
         id={id}
         type={type}
+        onKeyDown={onKeyDown}
         placeholder={placeholder}
         className="w-full rounded-lg border border-slate-300 bg-white px-3 py-2 text-slate-900 placeholder-slate-400 shadow-sm transition outline-none focus:border-slate-400 focus:ring-2 focus:ring-slate-200"
         {...register}
--- client/src/components/PrimaryBtn.tsx
+++ client/src/components/PrimaryBtn.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router";
+import ButtonLoading from "./ButtonLoading";
 
 interface PrimaryBtnProps {
   children: React.ReactNode;
@@ -6,6 +7,7 @@ interface PrimaryBtnProps {
   to?: string | undefined;
   disabled?: boolean;
   onClick?: () => void;
+  isPending?: boolean;
 }
 
 function PrimaryBtn({
@@ -13,6 +15,7 @@ function PrimaryBtn({
   className = "",
   to,
   disabled = false,
+  isPending = false,
   onClick,
 }: PrimaryBtnProps) {
   const Component = to ? Link : "button";
@@ -21,10 +24,10 @@ function PrimaryBtn({
     <Component
       to={to as string}
       onClick={() => onClick && onClick()}
-      disabled={disabled}
+      disabled={disabled || isPending}
       className={`bg-base border-base cursor-pointer border-3 px-6 py-3 text-white ${className} duration-300 hover:bg-white hover:text-black`}
     >
-      {children}
+      {isPending ? <ButtonLoading /> : children}
     </Component>
   );
 }
