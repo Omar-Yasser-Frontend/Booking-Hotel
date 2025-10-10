e3e868b521ad7cb11677b59ab12137dbca1591db
Omar-Yasser-Frontend <craft14716@gmail.com>
2025-10-10 17:53:14 +0300

    feat: adding controller for endpoint that return reserved dates for room

--- server/src/controllers/reservationController.js
+++ server/src/controllers/reservationController.js
@@ -11,6 +11,22 @@ export const getReservations = async (req, res) => {
   ResponseFormatter.success(res, { reservations: userReservations }, null, 200);
 };
 
+export const getReservedDates = async (req, res) => {
+  const reservedDates = await reservationService.getReservedDates(
+    req.params.roomId
+  );
+  console.log(req.params.roomId);
+  const buildReservedDates = reservedDates.map(
+    ({ _id, checkIn, checkOut }) => ({
+      _id,
+      from: checkIn,
+      to: checkOut,
+    })
+  );
+
+  ResponseFormatter.success(res, { dates: buildReservedDates });
+};
+
 export const cancelReservation = async (req, res) => {
   const reservation = await reservationService.cancelReservation(
     req.params.id,
