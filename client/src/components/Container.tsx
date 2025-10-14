79fd13577dc32f930afcdb6b1b8f4a0d510be581
Omar-Yasser-Frontend <craft14716@gmail.com>
2025-10-14 16:12:50 +0300

    refactor: adding new props to container

--- client/src/components/Container.tsx
+++ client/src/components/Container.tsx
@@ -1,6 +1,14 @@
-function Container({ children }: { children: React.ReactNode }) {
+function Container({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
   return (
-    <div className="relative container mx-auto h-full px-4 py-20">
+    <div
+      className={`relative container mx-auto h-full px-4 py-20 ${className}`}
+    >
       {children}
     </div>
   );
